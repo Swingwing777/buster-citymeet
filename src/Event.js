b044d83213b7d859d41a7c4be6996ce66291a230
David <haldw@live.co.uk>
2020-11-11 17:50:51 +0400

    Event testing 100%

--- src/Event.js
+++ src/Event.js
@@ -3,8 +3,45 @@
 import React, { Component } from "react";
 
 class Event extends Component {
-  render() {
-    return <div></div>;
+  state = {
+    showDetails: false
   }
-}
+
+  handleToggleDetails = () => {
+    this.setState(prevState => ({
+      showDetails: !prevState.showDetails
+    }))
+  };
+
+  render() {
+    const { event } = this.props;
+    if (!this.state.showDetails) {
+      return <div className='eventDetails'>
+        <div className='eventSummary'>{event.summary}</div>
+        <div className='eventStart'>{event.start.dateTime}</div>
+        <div className='eventTimezone'>{event.start.timeZone}</div>
+        <div className='eventLocation'>{event.location}</div>
+        <b
+          className='buttonDetails'
+          onClick={() => this.handleToggleDetails()}>
+          Details
+        </b>
+      </div>
+    }
+    return <div className='eventDetails'>
+      <div className='eventSummary'>{event.summary}</div>
+      <div className='eventStart'>{event.start.dateTime}</div>
+      <div className='eventTimezone'>{event.start.timeZone}</div>
+      <div className='eventLocation'>{event.location}</div>
+      <div className='eventDescription'>{event.description}</div>
+      <b
+        className='buttonDetails'
+        onClick={() => this.handleToggleDetails()}>
+        Hide Details
+        </b>
+    </div>;
+  }
+};
+
+
 export default Event;
---
+++ src/_tests_/Event.test.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Event from '../Event';
+// import { mockData } from '../mock-data';
+
+describe('<Event /> component', () => {
+  let EventWrapper;
+  beforeAll(() => {
+    EventWrapper = shallow(<Event event={{
+      kind: 'calendar#event',
+      summary: 'Learn JavaScript',
+      description: 'Have you wondered how you can ask Google to show you the list of the top ten must-see places in London? And how Google presents you the list? How can you submit the details of an application? Well, JavaScript is doing these.',
+      location: 'London, UK',
+      start: {
+        dateTime: '2020-05-19T16:00:00+02:00',
+        timeZone: 'Europe/Berlin'
+      },
+    }
+    } />);
+  });
+
+  beforeEach(() => {
+    EventWrapper.setState({ showDetails: false })
+  })
+
+  test('that event parent element exists', () => {
+    expect(EventWrapper.find('.eventDetails')).toHaveLength(1);
+  });
+  test('that one button element exists', () => {
+    expect(EventWrapper.find('.buttonDetails')).toHaveLength(1);
+  });
+  test('that Event state toggles on button click', () => {
+    const showDetailsState = EventWrapper.state('showDetails');
+    EventWrapper.find('.buttonDetails').at(0).simulate('click');
+    expect(EventWrapper.state('showDetails')).toBe(!showDetailsState);
+  });
+
+  test('that event.start/summary/location but not description display always', () => {
+    expect(EventWrapper.find('.eventSummary')).toHaveLength(1);
+    expect(EventWrapper.find('.eventStart')).toHaveLength(1);
+    expect(EventWrapper.find('.eventTimezone')).toHaveLength(1);
+    expect(EventWrapper.find('.eventLocation')).toHaveLength(1);
+    expect(EventWrapper.find('.eventDescription')).toHaveLength(0);
+  });
+
+  test('that event.description displays on first button click, hides on second', () => {
+    EventWrapper.find('.buttonDetails').at(0).simulate('click');
+    expect(EventWrapper.find('.eventSummary')).toHaveLength(1);
+    expect(EventWrapper.find('.eventStart')).toHaveLength(1);
+    expect(EventWrapper.find('.eventTimezone')).toHaveLength(1);
+    expect(EventWrapper.find('.eventLocation')).toHaveLength(1);
+    expect(EventWrapper.find('.eventDescription')).toHaveLength(1);
+    EventWrapper.find('.buttonDetails').at(0).simulate('click');
+    expect(EventWrapper.find('.eventDescription')).toHaveLength(0);
+  });
+  test('that Event description hidden by default', () => {
+    expect(EventWrapper.find('.eventDescription')).toHaveLength(0);
+  });
+});
+
