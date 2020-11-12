07a768156d6ef6f80207be0473811720d9bd6da6
David <haldw@live.co.uk>
2020-11-12 18:35:40 +0400

    Ex AA 4.4 - first 3 tests

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 
 class App extends Component {
+  state = {
+    events: [],
+    locations: []
+  }
   render() {
     return (
       <div className="App">
-        <CitySearch />
-        <EventList />
+        <CitySearch locations={this.state.locations} />
+        <EventList events={this.state.events} />
         <NumberOfEvents />
       </div>
     );
