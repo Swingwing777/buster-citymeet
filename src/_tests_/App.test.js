488d9d1f7aa93aec139ffc7c6d57127362c43e2d
David <haldw@live.co.uk>
2020-11-15 11:21:27 +0400

    Update React 16.14.0

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -26,3 +26,12 @@ describe('<App /> component', () => {
   });
 });
 
+describe('<App /> integration', () => {
+  test('App passes "events" state as a prop to EventList', () => {
+    const AppWrapper = mount(<App />);
+    const AppEventsState = AppWrapper.state('events');
+    expect(AppEventsState).not.toEqual(undefined);
+    expect(AppWrapper.find(EventList).props().events).toEqual(AppEventsState);
+    AppWrapper.unmount();
+  });
+});
