e440ab821be607644d17e2aa961463ff50942202
David <haldw@live.co.uk>
2020-11-15 11:30:27 +0400

    react-bootstrap added to App.js

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
+import { Container } from 'react-bootstrap'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <CitySearch />
-        <EventList />
-        <NumberOfEvents />
-      </div>
+        <Container>
+          <CitySearch />
+          <EventList />
+          <NumberOfEvents />
+        </Container>
+      </div >
     );
   }
 }
