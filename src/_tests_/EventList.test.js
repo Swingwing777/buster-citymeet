2e6c9aa39218dda9113b1d794ee74c989a08293a
David <haldw@live.co.uk>
2020-11-15 15:37:35 +0400

    Update EventList.test.js
    Comments only

--- src/_tests_/EventList.test.js
+++ src/_tests_/EventList.test.js
@@ -3,11 +3,13 @@ import { shallow } from 'enzyme';
 import EventList from '../EventList';
 import Event from '../Event';
 import { mockData } from '../_support_/mock-data';
-import { extractLocations } from '../_support_/api';
+// import { extractLocations } from '../_support_/api';
 
 describe('<EventList /> component', () => {
   test('render correct number of events', () => {
     const EventListWrapper = shallow(<EventList events={mockData} />);
+
+    // find() creates an array. Its length needs to match mockData.length
     expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
   });
 });
