3a1b43c573ea4da37bed7c3c4622568338c6f21f
David <haldw@live.co.uk>
2020-11-17 17:53:09 +0400

    filterEventsByCity added

---
+++ src/_features_/filterEventsByCity.test.js
@@ -0,0 +1,9 @@
+// src/_features_/filterEventsByCity.test.js
+
+import { loadFeature, defineFeature } from 'jest-cucumber';
+
+const feature = loadFeature('./filterEventsByCity.feature');
+
+defineFeature(feature, test => {
+
+});
