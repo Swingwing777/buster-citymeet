9f1453febfa9e5bbe7deed435702863edd1c4dd6
David <haldw@live.co.uk>
2020-11-27 18:56:32 +0400

    service-worker cloned from doner app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import * as atatus from 'atatus-spa';
 
 ReactDOM.render(
@@ -16,5 +17,10 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
 atatus.config('0ee536fcb8ee42fabaa91c6c5f1f0110').install();
-// atatus.notify(new Error('Test Atatus Setup'));
