98b902785bc92e4abaafc119da9d6106ce099de6
David <haldw@live.co.uk>
2020-12-02 15:47:34 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ class App extends Component {
     const { locations, events } = this.state;
     const data = locations.map((location) => {
       const number = events.filter((event) => event.location === location).length
-      const city = location.split(',').shift()
+      const city = location.split(', ' || ' ').shift()
       return { city, number };
     })
     return data;
